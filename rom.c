db164d2a4371bc9dfc606cafcb2883e48fdfda94
vext01 <vext01@gmail.com>
2012-01-23 15:06:31 +0000

    begin to parse in rom header

--- rom.c
+++ rom.c
@@ -1,10 +1,30 @@
+#include <stdio.h>
+
 #include "dgenmen.h"
 #include "rom.h"
 
 int
 gns_load_rom_header(struct gns_rom_header *hdr, char *file)
 {
-	return (GNS_OK);
+	FILE			*f;
+	int			 ret = GNS_FAIL;
+
+	if ((f = fopen(file, "r")) == NULL) {
+		warn("fopen");
+		goto clean;
+	}
+
+	printf("Loading %d bytes from %s\n", sizeof(struct gns_rom_header), file);
+	if ((fread(hdr, sizeof(struct gns_rom_header), 1, f))
+	    != sizeof(struct gns_rom_header)) {
+		warn("fread");
+		goto clean;
+	}
+
+	ret = GNS_OK;
+clean:
+	fclose(f);
+	return (ret);
 }
 
 void
