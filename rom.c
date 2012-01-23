e7f6b648a0c3a96183082ed99cc77aa9fd8dacd4
vext01 <vext01@gmail.com>
2012-01-23 21:50:45 +0000

    print what we know

--- rom.c
+++ rom.c
@@ -4,19 +4,19 @@
 #include "rom.h"
 
 struct gns_rom_header_mapping gns_rom_header_mappings[] = {
-	{GNS_OFS_CONSOLE_NAME,	GNS_FLEN_CONSOLE_NAME,	"Console Name"},
+	{GNS_OFS_CONSOLE,	GNS_FLEN_CONSOLE,	"Console Name"},
 	{GNS_OFS_COPYRIGHT,	GNS_FLEN_COPYRIGHT,	"Copyright"},
-	{GNS_OFS_GAME_NAME_DOMESTIC, GNS_FLEN_GAME_NAME_DOMESTIC, "Domestic Name"},
-	{GNS_OFS_GAME_NAME_OVERSEAS, GNS_FLEN_GAME_NAME_OVERSEAS, "Overseas Name"},
+	{GNS_OFS_NAME_DOMESTIC, GNS_FLEN_NAME_DOMESTIC, "Domestic Name"},
+	{GNS_OFS_NAME_OVERSEAS, GNS_FLEN_NAME_OVERSEAS, "Overseas Name"},
 	{GNS_OFS_TYPE,		GNS_FLEN_TYPE,		"Game Type"},
 	{GNS_OFS_PRODUCT_CODE,	GNS_FLEN_PRODUCT_CODE,	"Product Code"},
 	{GNS_OFS_CHECKSUM,	GNS_FLEN_CHECKSUM,	"Checksum"},
 	{GNS_OFS_IO_SUPPORT,	GNS_FLEN_IO_SUPPORT,	"IO Support"},
-	{GNS_OFS_ROM_CAPACITY,	GNS_FLEN_ROM_CAPACITY,	"ROM Capacity"},
+	{GNS_OFS_CAPACITY,	GNS_FLEN_CAPACITY,	"ROM Capacity"},
 	{GNS_OFS_RAM,		GNS_FLEN_RAM,		"RAM"},
 	{GNS_OFS_MODEM_DATA,	GNS_FLEN_MODEM_DATA,	"Modem Data"},
 	{GNS_OFS_MEMO,		GNS_FLEN_MEMO,		"Memo"},
-	{GNS_OFS_RELEASE_COUNTRY, GNS_FLEN_RELEASE_COUNTRY, "Release Country"},
+	{GNS_OFS_COUNTRY,	GNS_FLEN_COUNTRY,	"Release Country"},
 	{-1,			-1,			0}
 };
 
@@ -64,7 +64,10 @@ gns_nprint(char *str, size_t ct)
 void
 gns_print_rom_header(struct gns_rom_header *hdr)
 {
-	GNS_PRINT_HDR_FLD("Console Name", hdr->game_name_domestic, GNS_FLEN_GAME_NAME_DOMESTIC);
-	GNS_PRINT_HDR_FLD("Game Name (domestic)", hdr->game_name_domestic, GNS_FLEN_GAME_NAME_DOMESTIC);
-	GNS_PRINT_HDR_FLD("Game Name (overseas)", hdr->game_name_overseas, GNS_FLEN_GAME_NAME_OVERSEAS);
+	struct gns_rom_header_mapping		*f = gns_rom_header_mappings;
+
+	while (f->name != NULL) {
+		GNS_PRINT_HDR_FLD(f->name, (char *) hdr + f->offset, f->length);
+		f++;
+	}
 }
